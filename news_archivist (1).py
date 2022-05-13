becbfe2ee8584dab0323b9232d45fe56041dd5a6
Lane <89763058+CptR3dBeard@users.noreply.github.com>
2022-05-13 12:40:49 +1000

    Assignment Documentation

---
+++ news_archivist (1).py
@@ -0,0 +1,99 @@
+﻿
+#-----Statement of Authorship----------------------------------------#
+#
+#  This is an individual assessment item.  By submitting this
+#  code I agree that it represents my own work.  I am aware of
+#  the University rule that a student must not act in a manner
+#  which constitutes academic dishonesty as stated and explained
+#  in QUT's Manual of Policies and Procedures, Section C/5.3
+#  "Academic Integrity" and Section E/2.1 "Student Code of Conduct".
+#
+#    Student no: PUT YOUR STUDENT NUMBER HERE
+#    Student name: PUT YOUR NAME HERE
+#
+#  NB: Files submitted without a completed copy of this statement
+#  will not be marked.  Submitted files will be subjected to
+#  software plagiarism analysis using the MoSS system
+#  (http://theory.stanford.edu/~aiken/moss/). [1C2202]
+#
+#--------------------------------------------------------------------#
+
+
+
+#-----Task Description-----------------------------------------------#
+#
+#  News Archivist
+#
+#  In this task you will combine your knowledge of HTMl/XML mark-up
+#  languages with your skills in Python scripting, pattern matching
+#  and Graphical User Interface development to produce a useful
+#  application for maintaining and displaying archived news or
+#  current affairs stories on a topic of your own choice.  See the
+#  instruction sheet accompanying this file for full details.
+#
+#--------------------------------------------------------------------#
+
+
+
+#-----Imported Functions---------------------------------------------#
+#
+# Below are various import statements that were used in our sample
+# solution.  You should be able to complete this assignment using
+# these functions only.
+
+# Import the function for opening a web document given its URL.
+from urllib.request import urlopen
+
+# Import the function for finding all occurrences of a pattern
+# defined via a regular expression, as well as the "multiline"
+# and "dotall" flags.
+from re import findall, MULTILINE, DOTALL
+
+# A function for opening an HTML document in your operating
+# system's default web browser. We have called the function
+# "webopen" so that it isn't confused with the "open" function
+# for writing/reading local text files.
+from webbrowser import open as webopen
+
+# A module with useful functions on pathnames including:
+# normpath: function for 'normalising' a  path to a file to the path-naming
+# conventions used on this computer.  Apply this function to the full name
+# of your HTML document so that your program will work on any operating system.
+# exists: returns True if the supplied path refers to an existing path
+from os.path import *
+
+# An operating system-specific function for getting the current
+# working directory/folder.  Use this function to create the
+# full path name to your HTML document.
+from os import getcwd
+ 
+# Import the standard Tkinter GUI functions.
+from tkinter import *
+
+# Import the date and time function.
+# This module *may* be useful, depending on the websites you choose.
+# Eg convert from a timestamp to a human-readable date:
+# >>> datetime.fromtimestamp(1586999803) # number of seconds since 1970
+# datetime.datetime(2020, 4, 16, 11, 16, 43)
+from datetime import datetime
+
+#
+#--------------------------------------------------------------------#
+
+
+
+#-----Student's Solution---------------------------------------------#
+#
+# Put your solution at the end of this file.
+#
+
+# Name of the folder containing your archived web documents.  When
+# you submit your solution you must include the web archive along with
+# this Python program. The archive must contain one week's worth of
+# downloaded HTML/XML documents. It must NOT include any other files,
+# especially image files.
+internet_archive = 'InternetArchive'
+
+
+################ PUT YOUR SOLUTION HERE #################
+pass
